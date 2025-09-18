19f5042406c3d181fe036403c45a04b35e91345a
WaspishRaccoon <p.v.elesin@gmail.com>
2025-09-18 16:57:55 +0000

    fixes

--- hydrv_vectornav/example/src/main.cpp
+++ hydrv_vectornav/example/src/main.cpp
@@ -35,7 +35,7 @@ constinit hydrv::GPIO::GPIOLow rx_pin1(hydrv::GPIO::GPIOLow::GPIOB_port, 7,
 constinit hydrv::GPIO::GPIOLow tx_pin1(hydrv::GPIO::GPIOLow::GPIOB_port, 6,
                                        hydrv::GPIO::GPIOLow::GPIO_UART_TX);
 constinit hydrv::UART::UART<255, 255>
-    uart1(hydrv::UART::UARTLow::USART1_115200_LOW, rx_pin3, tx_pin3, 7);
+    uart1(hydrv::UART::UARTLow::USART1_115200_LOW, rx_pin1, tx_pin1, 7);
 
 constinit hydrolib::logger::LogDistributor distributor("[%s] [%l] %m\n\r",
                                                        uart3);
@@ -52,20 +52,16 @@ int main(void)
 {
     clock.Init();
     uart3.Init();
+    uart1.Init();
     
     NVIC_SetPriorityGrouping(0);
 
     distributor.SetAllFilters(0, hydrolib::logger::LogLevel::ERROR);
     distributor.SetAllFilters(1, hydrolib::logger::LogLevel::INFO);
-
-    uart1.Init();
     
     vector_nav.Reset();
     clock.Delay(500);
     vector_nav.Init();
-
-    
-
     // uart1.StartRx();
 
     unsigned last_log = 0;
