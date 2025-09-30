bdf458c6d09b15c86c71872d9b79656119c0223a
WaspishRaccoon <p.v.elesin@gmail.com>
2025-09-30 15:42:03 +0000

    fixed

--- hydrv_vectornav/example/src/main.cpp
+++ hydrv_vectornav/example/src/main.cpp
@@ -1,9 +1,14 @@
 #include <string.h>
+#include <sys/_intsup.h>
 #include <sys/_types.h>
 
 #include "stm32f407xx.h"
 #include "stm32f4xx.h"
 
+#include "hydrolib_log_distributor.hpp"
+#include "hydrolib_logger.hpp"
+#include "hydrv_clock.hpp"
+#include "hydrv_uart.hpp"
 #include "hydrv_vectornav.hpp"
 
 constinit hydrv::clock::Clock clock(hydrv::clock::Clock::HSI_DEFAULT);
@@ -13,19 +18,32 @@ constinit hydrv::GPIO::GPIOLow tx_pin3(hydrv::GPIO::GPIOLow::GPIOC_port, 10,
                                        hydrv::GPIO::GPIOLow::GPIO_UART_TX);
 constinit hydrv::UART::UART<255, 255>
     uart3(hydrv::UART::UARTLow::USART3_921600_LOW, rx_pin3, tx_pin3, 7);
+
+constinit hydrv::GPIO::GPIOLow rx_pin1(hydrv::GPIO::GPIOLow::GPIOB_port, 7,
+                                       hydrv::GPIO::GPIOLow::GPIO_UART_RX);
+constinit hydrv::GPIO::GPIOLow tx_pin1(hydrv::GPIO::GPIOLow::GPIOB_port, 6,
+                                       hydrv::GPIO::GPIOLow::GPIO_UART_TX);
+
 constinit hydrolib::logger::LogDistributor distributor("[%s] [%l] %m\n\r",
                                                        uart3);
+constinit hydrolib::logger::Logger logger1("VectorNAV", 0, distributor);
 constinit hydrolib::logger::Logger logger2("System", 1, distributor);
-
-hydrv::vectornav::VectorNAV vectornav;
 
 int main(void)
 {
-    clock.Init();
-    uart3.Init();
+    hydrv::vectornav::VectorNAV vector_nav(
+        hydrv::UART::UARTLow::USART1_115200_LOW, rx_pin1, tx_pin1, logger1);
+
+    clock.ConfigureHSI();
+    NVIC_SetPriorityGrouping(0);
 
     distributor.SetAllFilters(0, hydrolib::logger::LogLevel::ERROR);
     distributor.SetAllFilters(1, hydrolib::logger::LogLevel::INFO);
+
+    uart3.Init();
+    vector_nav.Reset();
+    clock.Delay(500);
+    vector_nav.Init();
 
     unsigned last_log = 0;
     unsigned counter = 0;
@@ -34,12 +52,12 @@ int main(void)
     unsigned packages = 0;
     while (1)
     {
-        vectornav.Process();
+        vector_nav.Process();
         if (clock.GetSystemTime() - last_log > 100)
         {
-            int yaw = vectornav.GetYaw();
-            int pitch = vectornav.GetPitch();
-            int roll = vectornav.GetRoll();
+            int yaw = static_cast<int>(vector_nav.GetYaw() * 100);
+            int pitch = static_cast<int>(vector_nav.GetPitch() * 100);
+            int roll = static_cast<int>(vector_nav.GetRoll() * 100);
 
             LOG(logger2, hydrolib::logger::LogLevel::INFO,
                 "yaw: {}.{}\tpitch: {}.{}\t roll: {}.{}", yaw / 100,
@@ -51,15 +69,17 @@ int main(void)
             counter++;
             if (counter == 50)
             {
-                unsigned next_wrong_crc = vectornav.GetWrongCRCCount();
-                unsigned next_rubbish_bytes = vectornav.GetRubbishBytesCount();
-                unsigned next_packages = vectornav.GetPackagesCount();
+                unsigned next_wrong_crc = vector_nav.GetWrongCRCCount();
+                unsigned next_rubbish_bytes = vector_nav.GetRubbishBytesCount();
+                unsigned next_packages = vector_nav.GetPackagesCount();
+
                 LOG(logger2, hydrolib::logger::LogLevel::INFO,
                     "Wrong crc: {}%, Rubbish bytes: {}%",
                     (next_wrong_crc - wrong_crc) * 100 /
                         (next_packages - packages),
                     (next_rubbish_bytes - rubbish_bytes) * 100 / 30 /
                         (next_packages - packages));
+                
                 wrong_crc = next_wrong_crc;
                 rubbish_bytes = next_rubbish_bytes;
                 packages = next_packages;
@@ -79,6 +99,7 @@ void Error_Handler(void)
 
 extern "C"
 {
+    void UART3IRQHandler() { uart3.IRQcallback(); }
     void SysTick_Handler(void) { clock.SysTickHandler(); }
 }
 
--- hydrv_vectornav/include/hydrv_vectornav.hpp
+++ hydrv_vectornav/include/hydrv_vectornav.hpp
@@ -1,43 +1,23 @@
 #pragma once
 
-#include "hydrolib_log_distributor.hpp"
-#include "hydrolib_logger.hpp"
 #include "hydrolib_vectronav.hpp"
-#include "hydrv_clock.hpp"
 #include "hydrv_uart.hpp"
-#include <concepts>
 #include <sys/_intsup.h>
 #include <sys/_types.h>
 
 namespace hydrv::vectornav
 {
-constinit hydrv::clock::Clock clock(hydrv::clock::Clock::HSI_DEFAULT);
-constinit hydrv::GPIO::GPIOLow rx_pin3(hydrv::GPIO::GPIOLow::GPIOC_port, 11,
-                                       hydrv::GPIO::GPIOLow::GPIO_UART_RX);
-constinit hydrv::GPIO::GPIOLow tx_pin3(hydrv::GPIO::GPIOLow::GPIOC_port, 10,
-                                       hydrv::GPIO::GPIOLow::GPIO_UART_TX);
-constinit hydrv::UART::UART<255, 255>
-    uart3(hydrv::UART::UARTLow::USART3_921600_LOW, rx_pin3, tx_pin3, 7);
-
-constinit hydrv::GPIO::GPIOLow rx_pin1(hydrv::GPIO::GPIOLow::GPIOB_port, 7,
-                                       hydrv::GPIO::GPIOLow::GPIO_UART_RX);
-constinit hydrv::GPIO::GPIOLow tx_pin1(hydrv::GPIO::GPIOLow::GPIOB_port, 6,
-                                       hydrv::GPIO::GPIOLow::GPIO_UART_TX);
-constinit hydrv::UART::UART<255, 255>
-    uart1(hydrv::UART::UARTLow::USART1_115200_LOW, rx_pin1, tx_pin1, 7);
-
-constinit hydrolib::logger::LogDistributor distributor("[%s] [%l] %m\n\r",
-                                                       uart3);
-constinit hydrolib::logger::Logger logger1("VectorNAV", 0, distributor);
-// constinit hydrolib::logger::Logger logger2("System", 1, distributor);
-
-hydrolib::VectorNAVParser vector_nav(uart1, logger1);
-
 class VectorNAV
 {
+private:
+    hydrolib::logger::Logger &logger_;
+    constinit hydrv::UART::UART<255, 255> uart_;
+
+    hydrolib::VectorNAVParser vector_nav_(uart_, logger_);
 
 public:
     void Init();
+    void Reset();
     void Process();
 
     unsigned GetYaw();
@@ -47,54 +27,65 @@ public:
     unsigned GetWrongCRCCount();
     unsigned GetRubbishBytesCount();
     unsigned GetPackagesCount();
+
+    void IRQCallback();
+
+public:
+    constexpr VectorNAV::VectorNAV(hydrv::UART::UARTLow preset,
+                                   hydrv::GPIO::GPIOLow &rx_pin,
+                                   hydrv::GPIO::GPIOLow &tx_pin,
+                                   hydrolib::logger::Logger &logger);
 };
+
+constexpr VectorNAV::VectorNAV(hydrv::UART::UARTLow preset,
+                               hydrv::GPIO::GPIOLow &rx_pin,
+                               hydrv::GPIO::GPIOLow &tx_pin,
+                               hydrolib::logger::Logger &logger)
+    : uart_(preset, rx_pin, tx_pin, 7), logger_(logger)
+{
+}
 
 void VectorNAV::Init()
 {
-    uart3.Init();
-    uart1.Init();
-
-    NVIC_SetPriorityGrouping(0);
-
-    distributor.SetAllFilters(0, hydrolib::logger::LogLevel::ERROR);
-    distributor.SetAllFilters(1, hydrolib::logger::LogLevel::INFO);
-
-    vector_nav.Reset();
-    clock.Delay(500);
-    vector_nav.Init();
+    uart_.Init();
+    vector_nav_.Init();
 }
 
-void VectorNAV::Process() { vector_nav.Process(); }
+void VectorNAV::Reset() { vector_nav_.Reset(); }
+
+void VectorNAV::Process() { vector_nav_.Process(); }
 
 unsigned VectorNAV::GetYaw()
 {
-    return static_cast<int>(vector_nav.GetYaw() * 100);
+    return static_cast<int>(vector_nav_.GetYaw() * 100);
 }
 
 unsigned VectorNAV::GetPitch()
 {
-    return static_cast<int>(vector_nav.GetPitch() * 100);
+    return static_cast<int>(vector_nav_.GetPitch() * 100);
 }
 
 unsigned VectorNAV::GetRoll()
 {
-    return static_cast<int>(vector_nav.GetRoll() * 100);
+    return static_cast<int>(vector_nav_.GetRoll() * 100);
 }
 
-unsigned VectorNAV::GetWrongCRCCount() { return vector_nav.GetWrongCRCCount(); }
+unsigned VectorNAV::GetWrongCRCCount()
+{
+    return vector_nav_.GetWrongCRCCount();
+}
 
 unsigned VectorNAV::GetRubbishBytesCount()
 {
-    return vector_nav.GetRubbishBytesCount();
+    return vector_nav_.GetRubbishBytesCount();
 }
 
-unsigned VectorNAV::GetPackagesCount() { return vector_nav.GetPackagesCount(); }
-
-extern "C"
+unsigned VectorNAV::GetPackagesCount()
 {
-    void USART3_IRQHandler(void) { uart3.IRQCallback(); }
-    void USART1_IRQHandler(void) { uart1.IRQCallback(); }
+    return vector_nav_.GetPackagesCount();
 }
+
+void VectorNAV::IRQCallback() { uart_.IRQCallback(); }
 
 void Error_Handler(void)
 {
