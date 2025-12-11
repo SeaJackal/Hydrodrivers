0b195a2cfad90774a4e7fb838bfa48860d7b6a32
klegot <redwolf.2007@mail.ru>
2025-12-11 18:30:45 +0000

    rebase

--- hydrv_shell/example/src/main.cpp
+++ hydrv_shell/example/src/main.cpp
@@ -6,16 +6,7 @@
 #include "hydrolib_device_manager.hpp"
 #include "hydrolib_echo.hpp"
 #include "hydrolib_shell.hpp"
-<<<<<<< HEAD
 #include "hydrolib_stream_device.hpp"
-=======
-
-#include "hydrolib_streambuf.hpp"
-
-#include <ostream>
-
-#include <ostream>
->>>>>>> 6b4beee (rebase)
 
 #define BUFFER_LENGTH 5
 
